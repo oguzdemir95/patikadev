7e37a514bd66de699ff83fd819357d6c31556603
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-05-09 19:00:56 +0300

    CS101

--- CS101/TipDonusum/obj/Debug/net8.0/TipDonusum.AssemblyInfo.cs
+++ CS101/TipDonusum/obj/Debug/net8.0/TipDonusum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TipDonusum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b7e858cb8fde4abb27b8f06f1469d64b8c1760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d267fc524da562995214c51c9ceb4a9d0962ab34")]
 [assembly: System.Reflection.AssemblyProductAttribute("TipDonusum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TipDonusum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
