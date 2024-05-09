ef317753e4ce32bf5892ebc77a1198398994ff68
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-05-09 09:56:56 +0300

    CS101

--- CS101/Operator/obj/Debug/net8.0/Operator.AssemblyInfo.cs
+++ CS101/Operator/obj/Debug/net8.0/Operator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2608b0a55ef3aa2f6f43782e4d3b6499294b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a301d45d893359be68c13c562d8a5b87577d4a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CS101/TipDonusum/obj/Release/net8.0/TipDonusum.AssemblyInfo.cs
+++ CS101/TipDonusum/obj/Release/net8.0/TipDonusum.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Degisken")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TipDonusum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a301d45d893359be68c13c562d8a5b87577d4a13")]
-[assembly: System.Reflection.AssemblyProductAttribute("Degisken")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Degisken")]
+[assembly: System.Reflection.AssemblyProductAttribute("TipDonusum")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TipDonusum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
