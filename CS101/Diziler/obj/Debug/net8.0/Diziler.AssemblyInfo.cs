95b89c52259ab62d34fffd442d93cb97108931cf
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-06-02 14:16:50 +0300

    CS101

--- CS101/Diziler/obj/Debug/net8.0/Diziler.AssemblyInfo.cs
+++ CS101/Diziler/obj/Debug/net8.0/Diziler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diziler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa8a399403cb838f9d58b81d09afb9441afd3e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58865ebe239b06f5931fe07037b357a152d7b12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diziler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diziler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
