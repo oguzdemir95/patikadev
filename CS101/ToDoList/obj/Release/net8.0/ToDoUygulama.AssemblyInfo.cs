a554f4d8756811d7d8298ef058984ed09691184f
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-06-16 00:42:02 +0300

    ToDo Uygulama Projesi

--- CS101/ToDoList/obj/Release/net8.0/ToDoUygulama.AssemblyInfo.cs
+++ CS101/ToDoList/obj/Release/net8.0/ToDoUygulama.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoUygulama")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fd0b65cfcc7b5ec4206d607857ca8ccaf03504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73841d27f44565ee15ba66dd1570b872b63e0662")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoUygulama")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoUygulama")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
